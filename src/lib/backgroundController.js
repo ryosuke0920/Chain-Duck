d6d482345a75aae66a8b89182bb8f2fa7ee6cfef
Ryosuke Ohta <mailryosuke@gmail.com>
2018-07-12 17:13:05 +0900

    open sidebar.

--- src/lib/backgroundController.js
+++ src/lib/backgroundController.js
@@ -12,26 +12,6 @@ export default class backgroundController extends controller
 		browser.browserAction.onClicked.addListener(this.browserActionBehavior.bind(this));
 	}
 	browserActionBehavior(e){
-		console.log("browserAction");
-		if( this.commentWindowId ) {
-			browser.windows.get( this.commentWindowId ).then((commentWindow)=>{
-				console.log(commentWindow);
-				browser.windows.remove(commentWindow.id);
-			}).catch((e)=>{
-				console.log(e);
-				console.log("already closed.");
-			});
-			this.commentWindowId = undefined;
-		}
-		let data = {
-		  "type": "normal",
-		  "url": "/comment/comment.html",
-		  "width": DEFAULT_COMMENT_WINDOW_WIDHT,
-			"left": screen.width - DEFAULT_COMMENT_WINDOW_WIDHT,
-		  "height": screen.height
-		};
-		browser.windows.create(data).then((commentWindow)=>{
-			this.commentWindowId = commentWindow.id;
-		});
+		let p = browser.sidebarAction.open();
 	}
 }
